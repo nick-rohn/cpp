ad7d8154501c4740a1bb336750caf44d5bf91bf5
NICKR0HN <tommaso.giussani01@gmail.com>
2023-11-14 19:35:08 +0100

    updated read

--- read.cc
+++ read.cc
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 
-#include "Particle.h"
 #include "Event.h"
 
 const Event* read( std::ifstream& file ){
@@ -11,40 +10,23 @@ const Event* read( std::ifstream& file ){
     int id;
 
     // try to read input file
-    // on success create new event
-    if ( file >> id) ev = new Event;
     // on failure return null pointer
-    else return nullptr;
+    if( !(file >> id) ) return nullptr;
 
-    // set event number
-    ev->ev_number = id;
-
-    // read and store decay coordinates
     double x, y, z;
     file >> x >> y >> z;
-    ev->x_decay = x;
-    ev->y_decay = y;
-    ev->z_decay = z;
+    ev = new Event( id, x, y, z );
 
-    // read and store number of particles
+    // read number of particles (could have used in the constructor)
     int n;
     file >> n;
-    ev->n_particles = n;
     
-    // allocate array for particles
-    ev->particles = new Particle*[n];
-
-    //create particle pointer and store data
+    //read and store particles
     int charge;
     double p_x, p_y, p_z;
     for( int i = 0; i < n; ++i ){
-        Particle* p = new Particle;
         file >> charge >> p_x >> p_y >> p_z;
-        p->charge = charge;
-        p->p_x = p_x;
-        p->p_y = p_y;
-        p->p_z = p_z;
-        ev->particles[i] = p;
+        ev->Add( charge, p_x, p_y, p_z );
     }
 
     return ev;
