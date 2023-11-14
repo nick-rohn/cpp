762c2b8ec7375f2b837022ffccaa29a086b6fe33
NICKR0HN <tommaso.giussani01@gmail.com>
2023-11-14 19:33:49 +0100

    created and implemented constants class

---
+++ Constants.h
@@ -0,0 +1,14 @@
+#ifndef constants_h
+#define constants_h
+
+class Constants{
+    public:
+        
+        static const double mass_pion;
+        static const double mass_proton;
+        static const double mass_k0;
+        static const double mass_lambda0;
+
+};
+
+#endif
