30b9f01b5d05873d0663b2b69aaa496c7c9c8008
NICKR0HN <tommaso.giussani01@gmail.com>
2023-11-14 19:35:31 +0100

    updated main

--- main.cc
+++ main.cc
@@ -2,14 +2,12 @@
 #include <fstream>
 #include <cmath>
 
+#include "MassMean.h"
+
 struct Event;
 
 const Event* read( std::ifstream& file );
 void dump( const Event& ev );
-void clear ( const Event* ev );
-bool add( const Event& ev,
-          double mass_min, double mass_max,
-          double& inv_mass_sum, double& mass_2_sum);
 
 
 int main( int argc, char *argv[] ){
@@ -20,30 +18,32 @@ int main( int argc, char *argv[] ){
     // event
     const Event* ev;
 
-    // cumulative variables
-    int n_events = 0;
-    double inv_mass_sum, mass_2_sum;
-    inv_mass_sum = mass_2_sum = 0;
+    // K0 and Lambda0 objects
+    MassMean kay0( 0.495, 0.500 );
+    MassMean lam0( 1.115, 1.116 );
 
     // loop over events
     while( (ev = read( file )) != nullptr ){
         dump( *ev );
-        if( add( *ev, 0.490, 0.505, inv_mass_sum, mass_2_sum )) ++n_events;
-        // could create global variables for min and max masses, and simplify add() input
-        // but in future versions they will change depending on the event
-        clear( ev );
+        kay0.add( *ev );
+        lam0.add( *ev );
+        delete ev;
     }
 
-    // compute mean mass and rms
-    double mean_mass = inv_mass_sum / n_events;
-    double rms_mass = ( mass_2_sum / n_events ) - ( mean_mass * mean_mass );
-    rms_mass = ( rms_mass > 0 ? sqrt(rms_mass) : 0.0 );
-    // could have used function to keep main simple, but it's not recursive and will be changed in the future
+    // compute results
+    kay0.compute();
+    lam0.compute();
 
     // print results
     std::cout << std::endl;
-    std::cout << mean_mass << " " << rms_mass << std::endl;
-    std::cout << n_events << std::endl;
+    std::cout << "K0:\t\t" << kay0.MassAvg() << ' '
+                           << kay0.MassRms() << '\t'
+                           << kay0.NEvents() << std::endl;
+    
+    std::cout << "Lambda0:\t" << lam0.MassAvg() << ' '
+                              << lam0.MassRms() << '\t'
+                              << lam0.NEvents() << std::endl;
+
 
     return 0;
     
