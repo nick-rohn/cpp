51cd846e48030f41c24543eb57a021207aabed34
NICKR0HN <tommaso.giussani01@gmail.com>
2023-12-28 12:54:00 +0100

    implemented EventDump.cc

---
+++ EventDump.cc
@@ -0,0 +1,49 @@
+#include "EventDump.h"
+#include "Event.h"
+
+#include <iostream>
+
+using namespace std;
+
+EventDump::EventDump() {
+}
+
+
+EventDump::~EventDump() {
+}
+
+
+// function to be called at execution start
+void EventDump::BeginJob() {
+  return;
+}
+
+
+// function to be called at execution end
+void EventDump::EndJob() {
+  return;
+}
+
+
+// function to be called for each event
+void EventDump::Process( const Event& ev ) {
+
+    // write event data
+    cout << ev.EventNumber() << ' '
+         << ev.XDecay() << ' ' << ev.YDecay() << ' ' << ev.ZDecay() << ' ' 
+         << ev.NParticles();
+    
+    // write particles
+    for ( Event::u_int i = 0; i < ev.NParticles(); ++i ){
+        Event::part_ptr p = ev.GetParticle(i);
+        cout << ' '
+             << p->charge << ' '
+             << p->p_x << ' ' << p->p_y << ' ' << p->p_z;
+    }
+
+    cout << endl;
+
+    return;
+
+}
+
