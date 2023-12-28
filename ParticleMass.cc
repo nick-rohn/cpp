d0b458297a51ab9931eca1189e1b9c40f9403211
NICKR0HN <tommaso.giussani01@gmail.com>
2023-12-28 14:12:55 +0100

    created and implemented ParticleMass class

---
+++ ParticleMass.cc
@@ -0,0 +1,51 @@
+#include "ParticleMass.h"
+
+#include <iostream>
+
+#include "MassMean.h"
+
+using namespace std;
+
+ParticleMass::ParticleMass() {
+}
+
+ParticleMass::~ParticleMass() {
+}
+
+
+// function to be called at execution start
+void ParticleMass::BeginJob() {
+
+    // create list of particles
+    p_list.reserve( 2 );
+    // K0
+    p_list.push_back( new MassMean( 0.495, 0.500 ) );
+    // Lambda0
+    p_list.push_back( new MassMean( 1.115, 1.116 ) );
+
+    return;
+
+}
+
+
+// function to be called at execution end
+void ParticleMass::EndJob() {
+
+    // loop over elements
+    for( MassMean* particle: p_list ) {
+        // compute results
+        particle->compute();
+        // print results
+        cout << endl;
+        cout << particle->MassAvg() << ' '
+             << particle->MassRms() << '\t'
+             << particle->NEvents() << endl;
+    }
+}
+
+
+// function to be called for each event
+void ParticleMass::Process( const Event& ev ) {
+  // loop over particles and pass event to each of them
+  for( MassMean* particle: p_list ) particle->add( ev );
+}
