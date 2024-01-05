248674ad13ff8c5e54778eb1f0e91fd3376c2b3e
NICKR0HN <tommaso.giussani01@gmail.com>
2024-01-05 20:12:10 +0100

    added speed of light to Constants

--- Constants.cc
+++ Constants.cc
@@ -5,3 +5,4 @@ const double Constants::mass_pion    = 0.1395706;   // GeV/c^2
 const double Constants::mass_proton  = 0.938272;    // GeV/c^2
 const double Constants::mass_k0      = 0.497611;    // GeV/c^2
 const double Constants::mass_lambda0 = 1.115683;    // GeV/c^2
+const double Constants::light_speed  = 0.029979246; // cm/ps
